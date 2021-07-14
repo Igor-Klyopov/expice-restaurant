bde9564876f334858e21c5808f017774d871b132
Igor Klyopov <shoot8202@gmail.com>
2021-07-14 16:33:38 +0300

    removes comments from btn.js

--- js/btn.js
+++ js/btn.js
@@ -1,16 +1,12 @@
 window.addEventListener('scroll', function () {
-        // Если прокрутили дальше 599px, показываем кнопку
-        if (pageYOffset > 100) {
-            btn.classList.add('show');
-            // Иначе прячем
-        } else {
-            btn.classList.remove('show');
-        }
-    });
+    if (pageYOffset > 100) {
+        btn.classList.add('show');
 
-    // При клике прокручиываем на самый верх
-    btn.onclick = function (click) {
-        click.preventDefault();
-        scrollTo(0, 400);
+    } else {
+        btn.classList.remove('show');
     }
 });
+btn.onclick = function (click) {
+    click.preventDefault();
+    scrollTo(0, 400);
+};
